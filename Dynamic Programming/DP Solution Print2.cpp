b7f2a514add5d2a4fd2c1b4b1a59145a2099feed
SabbirHasanBhuiyan <81769766+SabbirHasanBhuiyan@users.noreply.github.com>
2022-06-17 17:55:37 +0600

    Create DP Solution Print2.cpp

---
+++ Dynamic Programming/DP Solution Print2.cpp
@@ -0,0 +1,189 @@
+/*
+https://codeforces.com/contest/1341/problem/D
+*/
+
+//Bismillah
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define endl '\n'
+#define all(a) (a).begin(),(a).end()
+#define sz(x) (int)x.size()
+#define mid(l,r) ((r+l)/2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define gcd(a,b) __gcd(a,b)
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p&1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, MOD-2); }
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+int dx[] = {0, 0, +1, -1};
+int dy[] = {+1, -1, 0, 0};
+//int dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
+//int dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
+
+
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+            return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+            os << "{";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << it -> first << " = " << it -> second ;
+                                            }
+                    return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+void faltu () {
+            cerr << endl;
+}
+
+template <typename T>
+void faltu( T a[], int n ) {
+            for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
+                cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) {
+            cerr << arg << ' ';
+                faltu(rest...);
+}
+
+const int mx=2001;
+string digit[]={"1110111", "0010010", "1011101", "1011011", "0111010", "1101011", "1101111", "1010010", "1111111", "1111011"};
+string num[mx];
+int n,k,dp[mx][mx];
+struct info{
+    int i,c,ans;
+    info(){}
+    info(int i,int c,int ans) : i(i),c(c),ans(ans) {}
+}dir[mx][mx];
+
+int getVal(string &s1,string &s2)
+{
+    int cost=0;
+    for(int i=0;i<s1.size();i++){
+        if(s1[i]=='1' && s2[i]=='0')    return -1;
+        else if(s1[i]=='0' && s2[i]=='1') cost++;
+    }
+    return cost;
+}
+
+bool solve(int i,int k)
+{
+    if(i>n) return k==0;
+    if(dp[i][k]!=-1)    return dp[i][k];
+
+    int cost;
+    for(int j=9;j>=0;j--){
+        cost=getVal(num[i],digit[j]);
+        if(cost==-1)    continue;
+
+        if(cost<=k){
+            if(solve(i+1,k-cost)){
+                dir[i][k]=info(i+1,k-cost,j);
+                return dp[i][k]=1;
+            }
+        }
+    }
+    return dp[i][k]=0;
+}
+
+void print(int i,int k)
+{
+    if(i==(n+1))    return;
+    cout<<dir[i][k].ans;
+    print(dir[i][k].i,dir[i][k].c);
+}
+int main()
+{
+    optimize();
+
+    cin>>n>>k;
+    for(int i=1;i<=n;i++)   cin>>num[i];
+    memset(dp,-1,sizeof dp);
+
+    if(solve(1,k)){
+        print(1,k);
+    }else cout<<"-1"<<endl;
+
+    return 0;
+}
+
