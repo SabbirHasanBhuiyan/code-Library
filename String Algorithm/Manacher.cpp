46a1f51143afb2144d21b8b331c7cbea3b6e08cd
SabbirHasanBhuiyan <81769766+SabbirHasanBhuiyan@users.noreply.github.com>
2024-09-14 12:01:34 +0600

    Create Manacher.cpp

---
+++ String Algorithm/Manacher.cpp
@@ -0,0 +1,27 @@
+
+void manacher(string &st)
+{
+    string s;
+    for(auto &ch: st){
+        s.PB('#');
+        s.PB(ch);
+    }
+    s.PB('#');
+    int n=s.size();
+    s="$"+s+"^";
+    std::vector<int> p(n+2);
+    int l=1,r=1; //s[l+1]...s[r-1] is the rightmost palindrome
+    for(int i=1;i<=n;i++){
+        if(r>=i)    p[i]=min(r-i,p[l+(r-i)]);
+        while(s[i-p[i]]==s[i+p[i]]){
+            p[i]++;
+        }
+        if((i+p[i])>r){
+            l=i-p[i],r=i+p[i];
+        }
+    }
+    for(int i=1;i<=n;i++)   p[i]--;
+}
+//relation with the actual 0 indexed string, st in respect to the vector, p
+//p[i*2+1] is the lenth of the even palindromic sub-string where center is between st[i] and st[i-1]  
+//p[i*2+2] is the lenth of the odd palindromic sub-string where center is st[i]
