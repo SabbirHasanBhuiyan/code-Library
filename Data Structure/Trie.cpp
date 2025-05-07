ca897471d160782259eef8548e8a9bea2c62fabd
SabbirHasanBhuiyan <81769766+SabbirHasanBhuiyan@users.noreply.github.com>
2025-05-07 21:36:24 +0600

    Update Trie.cpp

--- Data Structure/Trie.cpp
+++ Data Structure/Trie.cpp
@@ -1,81 +1,84 @@
-//Bismillah
-
 #include<bits/stdc++.h>
 using namespace std;
 
-#define int long long
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int,int> pii;
+const int N = 3e5 + 9;
 
-#define PB push_back
-#define F first
-#define S second
-#define endl '\n'
-#define all(a) (a).begin(),(a).end()
-#define sz(x) (int)x.size()
-#define mx_int_prime 999999937
-
-const double PI = acos(-1);
-const double eps = 1e-9;
-const int inf = 2000000000;
-const ll infLL = 9000000000000000000;
-#define MOD 1000000007
-
-#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
-
-const int SIZE=26;
-struct  node
-{
-    int count;
-    bool endOFWord;
-
-    node *ptr[SIZE]; //each node need pointer to atmost k child
-};
-
-node* getNode()
-{
-    node* tempNodePtr=new node;
-    tempNodePtr->endOFWord=false;
-
-    tempNodePtr->count=0;
-    for(int i=0;i<SIZE;i++) tempNodePtr->ptr[i]=NULL;
-    return tempNodePtr;
-}
-
-void insert(node *root,string &st){
-    node *tempNodePtr=root;
-
-    for(int i=0;i<(int)st.size();i++){
-        int idx=st[i]-'a';
-
-        if(tempNodePtr->ptr[idx]==NULL){
-            tempNodePtr->ptr[idx]=getNode();
-        }
-        tempNodePtr=tempNodePtr->ptr[idx];
+struct Trie {
+  static const int B = 31;//nofbits in number..
+  struct node {
+    node* nxt[2];
+    int sz;
+    node() {
+      nxt[0] = nxt[1] = NULL;
+      sz = 0;
     }
-
-    tempNodePtr->endOFWord=true;
-}
-
-bool search(node* root,string &st )
-{
-    node* tempNodePtr=root;
-    for(int i=0;i<(int)st.size();i++){
-        int idx=st[i]-'a';
-        if(tempNodePtr->ptr[idx]==NULL) return false;
-        else tempNodePtr=tempNodePtr->ptr[idx];
+  }*root;
+  Trie() {
+    root = new node();
+  }
+  void insert(int val) {
+    node* cur = root;
+    cur -> sz++;
+    for (int i = B - 1; i >= 0; i--) {
+      int b = val >> i & 1;
+      if (cur -> nxt[b] == NULL) cur -> nxt[b] = new node();
+      cur = cur -> nxt[b];
+      cur -> sz++;
     }
-    return true;
-}
-
-signed main()
-{
-    optimize();
-    #ifndef ONLINE_JUDGE
-    file();
-    #endif
-
-    return 0;
+  }
+  int query(int x, int k) { // number of values s.t. val ^ x < k
+    node* cur = root;
+    int ans = 0;
+    for (int i = B - 1; i >= 0; i--) {
+      if (cur == NULL) break;
+      int b1 = x >> i & 1, b2 = k >> i & 1;
+      if (b2 == 1) {
+        if (cur -> nxt[b1]) ans += cur -> nxt[b1] -> sz;
+        cur = cur -> nxt[!b1];
+      } else cur = cur -> nxt[b1];
+    }
+    return ans;
+  }
+  int get_max(int x) { // returns maximum of val ^ x
+    node* cur = root;
+    int ans = 0;
+    for (int i = B - 1; i >= 0; i--) {
+      int k = x >> i & 1;
+      if (cur -> nxt[!k]) cur = cur -> nxt[!k], ans |= (1LL<<i);
+      else cur = cur -> nxt[k];
+    }
+    return ans;
+  }
+  int get_min(int x) { // returns minimum of val ^ x
+    node* cur = root;
+    int ans = 0;
+    for (int i = B - 1; i >= 0; i--) {
+      int k = x >> i & 1;
+      if (cur -> nxt[k]) cur = cur -> nxt[k];
+      else cur = cur -> nxt[!k], ans |= (1LL<<i);
+    }
+    return ans;
+  }
+  void del(node* cur) {
+    for (int i = 0; i < 2; i++) if (cur -> nxt[i]) del(cur -> nxt[i]);
+    delete(cur);
+  }
+} t;
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int n, k;
+  cin >> n >> k;
+  int cur = 0;
+  long long ans = 1LL * n * (n + 1) / 2;
+  t.insert(cur);
+  for (int i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    cur ^= x;
+    ans -= t.query(cur, k);
+    t.insert(cur);
+  }
+  cout << ans << '\n';
+  return 0;
 }
